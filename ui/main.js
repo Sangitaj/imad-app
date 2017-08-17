23f2cabde020b6b3ce6f79b967fd46297218ac77
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-17 17:02:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,8 +8,8 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft+10;
-    imag.style.marginLeft = marginLeft + 'px';
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
