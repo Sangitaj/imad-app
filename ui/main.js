cfb4c2ffac38fc2765fc4c177b84c11d22e53004
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-18 13:01:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,14 @@
-console.log('Loaded!');
-
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New Value';
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+//Counter code
+var button = document.getElementById("counter");
+var counter = 0;
+button.onclick = function(){
+   
+   //Make a request to the counter endpoint.
+   
+   //Capture the response and store it in a variable.
+   
+   //Render the variable  into correct span.
+   counter = counter + 1;
+   var span = document.getElementById("counter");
+   span.innerHTML = counter.toString();
 };
