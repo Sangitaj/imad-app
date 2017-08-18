01712ad9a72e9c967204b9743fa32f5c2b8a8c7e
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-18 17:26:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,21 +40,17 @@ submit.onclick = function() {
                 for(var i=0; i<names.length; i++){
                      list == '<li>' + names[i] +'</li>';
             
-                }
+                    }
           var ul = document.getElementById('namelist');
               ul.innerHTML = list;
-         }
-       }
+                }
+            }
        //Not done yet
-   };
+        };
    //Make the request
    var nameInput = document.getElementById('name');
    var name = nameInput.value;
    request.open('GET','http://sangitajhunjhunwala.imad.hasura-app.io/submit-name?name=' + name,true);
    request.send(null);
    };
-   
-   var ul = document.getElementById('namelist');
-   ul.innerHTML = list;
-}
    
