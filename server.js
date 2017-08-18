63810babf60222a1e042761619ce380ed6776d4b
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-18 16:06:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,15 @@ app.get('/counter', function(req, res){
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/', function (req, res) {
+    //Get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName',function (req, res){
    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -116,15 +125,6 @@ app.get('/ui/main.js', function (req, res) {
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name/', function (req, res) {
-    //Get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    res.send(JSON.stringify(names));
 });
 
 
