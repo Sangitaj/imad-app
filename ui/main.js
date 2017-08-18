06ae1aca71524a0ae39bb06d576ab603bc28d8d3
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-18 17:01:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,24 +28,23 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
    //Make a request  to the server and send the name.
     //Capture the list of names and render it as a list.
-   var names = request.responseText;
-   names = JSON.parse(names);
-   var list ='';
-   for(var i=0; i<names.length; i++){
-       list == '<li>' + names[i] +'</li>';
    }
     //Create a request object.
    var request = new XMLHttpRequest();
-   
    //Capture the response and store it in a variable.
    request.onreadystatechange = function(){
        if(request.readyState === XMLHttpRequest.DONE){
           // Take some action
          if(request.status === 200){
-             var counter = request.responseText;
-             var span = document.getElementById('count');
-             span.innerHTML = counter.toString();
+             var names = request.responseText;
+             names = JSON.parse(names);
+             var list ='';
+                for(var i=0; i<names.length; i++){
+                     list == '<li>' + names[i] +'</li>';
+            
          }
+          var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
        }
        //Not done yet
    };
