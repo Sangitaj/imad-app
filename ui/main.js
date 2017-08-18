64d1c6895acfcb4816c7bf76626925aca9a4bcc4
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-18 16:38:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick = function(){
    request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
    //Make a request  to the server and send the name.
@@ -51,6 +50,8 @@ submit.onclick = function() {
        //Not done yet
    };
    //Make the request
+   var nameInput = document.getElementById('name');
+   var name = nameInput.value;
    request.open('GET','http://sangitajhunjhunwala.imad.hasura-app.io/submit-name?name=' + name,true);
    request.send(null);
 };
@@ -58,3 +59,4 @@ submit.onclick = function() {
    var ul = document.getElementById('namelist');
    ul.innerHTML = list;
    };
+   
