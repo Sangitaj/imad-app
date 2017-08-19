120be770d2c8820bbc0095ab0b167642ddcc6029
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-19 15:46:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,8 @@ button.onclick = function(){
              var counter = request.responseText;
              var span = document.getElementById('count');
              span.innerHTML = counter.toString();
-         }
-       }
+            }
+        }
        //Not done yet
     };
    //Make the request
