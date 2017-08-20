d757fec1809ba9ac61da92b8ca4d4bf0e8a9c472
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-20 23:51:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,35 +71,34 @@ function createTemplate (data) {
    var content = data.content;
    
    var htmlTemplate = `
-   <html>
-    <head>
-        <title>
-        
-          ${title}
-        </title>
-        <meta name="viewport" content="width-device-width,initial scale=1" />
+    <html>
+       <head>
+         <title>
+             ${title}
+         </title>
+          <meta name="viewport" content="width-device-width,initial scale=1" />
              <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date.toDateString()}
-        </div>
-        <div>
-            ${content}
-        </div>
-        </div>
-    </body>
+       </head>
+       <body>
+          <div class="container">
+              <div>
+                 <a href="/">Home</a>
+              </div>
+            <hr/>
+            <h3>
+                 ${heading}
+            </h3>
+            <div>
+                 ${date}
+            </div>
+            <div>
+                 ${content}
+            </div>
+          </div>
+        </body>
     </html>
     `;
-     return htmlTemplate;
+      return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
