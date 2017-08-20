a2c13fbd4455cdd9d12599e172a543419fd39e64
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-21 00:08:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ var articles = {
 
 function createTemplate (data) {
     
-   var title = data.title;
+   var Title = data.title;
    var date = data.date;
    var heading = data.heading;
    var content = data.content;
