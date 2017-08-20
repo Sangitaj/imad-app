456b5c502df0e76bbc3402e766b7d470420ce15d
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-21 00:05:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.get('/submit-name', function (req, res) {  //URL: /submit-name?namexxxx
 
 app.get('/:articleName',function (req, res){
    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  res.send(createTemplate(article[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
