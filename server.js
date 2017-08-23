86d7eb15ca511940e4119f91a5dcddc11c41eda7
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-23 15:00:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/articles/:articleName',function (req, res){
            res.status(500).send(err.toString());
        } else {
            if(result.rows.length === 0){
-               res.send(404).send('Article Not Found');
+               res.status(404).send('Article Not Found');
            } else {
                var articleData = result.rows(0);
                res.send(createTemplate(articleData));
