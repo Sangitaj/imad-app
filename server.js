60520fd87b93a49ae23b4308460180585929964d
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-08-23 15:16:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/submit-name', function (req, res) {  //URL: /submit-name?namexxxx
 });
 
 app.get('/articles/:articleName',function (req, res){
-    // SELECT * FROM article WHERE title = 'article-one'
+    // SELECT * FROM article WHERE title = '';DELETE WHERE 'a'= 'asdf'
    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
        if(err){
            res.status(500).send(err.toString());
