956fcccfe94511f75261ce9015b6e4edd454aaed
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-09-06 00:24:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,6 +29,6 @@ submit.onclick = function() {
    console.log(password);
    request.open('POST','http://sangitajhunjhunwala.imad.hasura-app.io/login', true);
    request.setRequestHeader('Content-Type', 'application/json');
-   request.send(JSON.stringify({username: username, password: password}));
+   request.submit.onclick(JSON.stringify({username: username, password: password}));
    };
    
