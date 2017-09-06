29a69c20a2d54ef98d2ee63dfbfb50a77ec74aa1
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-09-06 10:13:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,6 @@ submit.onclick = function() {
             //Not done yet
         };
    //Make the request
-   
    var username = document.getElementById('username').value;
    var password = document.getElementById('password').value;
    console.log(username);
