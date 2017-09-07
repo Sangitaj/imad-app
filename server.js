6aa470ba6aa73078da1c0e8f568e1efb47e3ed40
Sangita Jhunjhunwala <30897822+Sangitaj@users.noreply.github.com>
2017-09-07 10:19:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/hash/:input', function(req, res){
 
 app.post('/create-user', function(req, res) {
     //username,password
-    //{"username": "sangita","password": "password"}
+    //{"username": "sangita","password": "passwoord"}
     //JSON
     var username = req.body.username;
     var password = req.body.password;
